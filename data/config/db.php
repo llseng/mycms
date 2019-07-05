79093071ba81a701454a28d9e6e2ebe683cb9dc6
llseng <1300904522@qq.com>
2019-07-05 10:42:49 +0800

    服务器配置

--- data/config/db.php
+++ data/config/db.php
@@ -3,8 +3,8 @@ return array(
     /* 数据库配置 */
 	'DB_TYPE' => 'mysql',
 	'DB_HOST'=>'127.0.0.1',
-	'DB_USER'=>'duxcms',
-	'DB_PWD'=>'123456',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'qq1300904522',
 	'DB_PORT'=>3306,
 	'DB_NAME'=>'duxcms',
 	'DB_CHARSET' => 'utf8',
--- data/config/performance.php
+++ data/config/performance.php
@@ -4,6 +4,6 @@ return array (
 	'DEBUG'=>true,	//是否开启调试模式
 	'LOG_ON'=>true,	//是否开启出错信息保存到文件
 	'REWRITE_ON'=>false,	//伪静态开关
-	'COOKIE_PREFIX'=>'6mOT6_', // COOKIE前缀
-    'SAFE_KEY'=>'GFbeJwiQiPh1IokHbU8D', // 加密码
+	'COOKIE_PREFIX'=>'duxcms_', // COOKIE前缀
+    'SAFE_KEY'=>'B9qDqD4nSQoj7FHEhSr3', // 加密码
 );
